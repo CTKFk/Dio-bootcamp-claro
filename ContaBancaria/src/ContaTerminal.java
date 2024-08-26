b9a2a048faad1636cf684f5f3d4f995dc729009d
TK <caiotkfukuda@gmail.com>
2024-08-26 19:49:51 -0300

    Desafio DIO

---
+++ ContaBancaria/src/ContaTerminal.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        try (Scanner conta = new Scanner(System.in)) {
+            System.out.print("Por favor, digite o número da Agência: ");
+            String agencia = conta.nextLine();
+            
+            System.out.print("Por favor, digite o número da Conta: ");
+            int numero = conta.nextInt();
+            conta.nextLine();
+            
+            System.out.print("Por favor, digite o seu nome: ");
+            String nomeCliente = conta.nextLine();
+            
+            System.out.print("Por favor, digite o saldo inicial: ");
+            double saldo = conta.nextDouble();
+            
+            
+            System.out.printf("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                    + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
+        }
+    }
+}
